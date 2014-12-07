128ccd977bf3394e65512a3e53f705a1afa4aa2b
Gerald Bauer <gerald.bauer@gmail.com>
2014-12-07 18:16:48 +0100

    add at

---
+++ at.rb
@@ -0,0 +1,11 @@
+##########################################
+# Datafile for Austrian Beers 'n' Breweries
+#
+#  use
+#    $ beerdb new at
+
+world  'openmundi/world.db', setup: 'countries'
+world  'openmundi/austria.db'
+
+beer 'openbeer/at-austria'
+
