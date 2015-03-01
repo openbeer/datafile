ebd9b41e4a7939d41235dd3b02d77956eff6ef2c
Gerald Bauer <gerald.bauer@gmail.com>
2015-03-01 14:50:45 +0100

    add czech republic

---
+++ cz.rb
@@ -0,0 +1,10 @@
+##########################################
+# Datafile for Czech Beers 'n' Breweries
+#
+#  use
+#    $ beerdb new cz
+
+world  'openmundi/world.db', setup: 'europe'
+
+beer 'openbeer/cz-czech-republic'
+
