cf6b671e652f9f65a58af3595d2af6a0c2ae42a9
Gerald Bauer <gerald.bauer@gmail.com>
2015-03-04 11:46:52 +0100

    Create europe.rb

---
+++ europe.rb
@@ -0,0 +1,12 @@
+###############################################
+# Datafile for Beers 'n' Breweries in Europe
+#
+#  use
+#    $ beerdb new europe
+
+world  'openmundi/world.db', setup: 'europe'
+world  'openmundi/austria.db'
+
+beer 'openbeer/at-austria'
+beer 'openbeer/be-belgium'
+beer 'openbeer/cz-czech-republic'
