551e557238351f4cc32cf9f21d6ca52b7f9f450d
Gerald Bauer <gerald.bauer@gmail.com>
2015-03-05 08:40:06 +0100

    up

---
+++ bayern.rb
@@ -0,0 +1,11 @@
+############################################################
+# Datafile for Bavarian (Bayrische) Beers 'n' Breweries
+#
+#  use
+#    $ beerdb new bayern
+
+world  'openmundi/world.db', setup: 'countries'
+world  'openmundi/deutschland.db'
+
+beer 'openbeer/by-bayern'
+
---
+++ franken.rb
@@ -0,0 +1,11 @@
+###########################################################################
+# Datafile for Franconian (Fraenkische) Beers 'n' Breweries in Bavaria
+#
+#  use
+#    $ beerdb new franken
+
+world  'openmundi/world.db', setup: 'countries'
+world  'openmundi/deutschland.db'
+
+beer 'openbeer/franken'
+
