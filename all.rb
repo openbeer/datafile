1d14539c32649e7464ecdd4dfcc0c745e599f4bd
Gerald Bauer <gerald.bauer@gmail.com>
2015-04-06 18:45:29 +0200

    add all

---
+++ all.rb
@@ -0,0 +1,18 @@
+##########################################
+# Datafile for World Beer Book
+#
+#  use
+#    $ beerdb new all
+
+#
+# check/rename from all to book - why, why not??
+
+
+world  'openmundi/world.db'
+world  'openmundi/austria.db'
+
+
+beer 'openbeer/at-austria'
+beer 'openbeer/be-belgium'
+beer 'openbeer/cz-czech-republic'
+
